0c2448bc3fd88ad6caf12bd6196d51a027bf8055
yuna0102 <mkiyg@naver.com>
2020-05-12 02:02:04 +0900

    detail-new clear, new base inheritance

--- blogProject/blog/models.py
+++ blogProject/blog/models.py
@@ -5,6 +5,8 @@ class Blog(models.Model): # Blog 라는 이름의 객체 틀(Model) 생성
     title = models.CharField(max_length=200) # title 라는 최대 200 글자의 문자 데이터 저장
     pub_date = models.DateTimeField('date published') # pub_date 라는 날짜 시간 데이터 저장
     body = models.TextField() # content 라는 줄글 문자 저장
+    author = models.CharField(default='', max_length=10)
+
 
     # 이 객체를 가르키는 말을 title로 정하겠다
     def __str__(self):
